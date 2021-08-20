a3ce1ab03edfaff6493ad1fdd642c5dbe824817f
CyforSkadi <86471817+CyforSkadi@users.noreply.github.com>
2021-08-20 16:58:48 +0800

    Add files via upload

---
+++ 01 for_each.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+#include<algorithm>
+#include<vector>
+
+//for_each(iterator beg, iterator end, _func);
+// 遍历算法 遍历容器元素
+// beg 开始迭代器
+// end 结束迭代器
+// _func 函数或者函数对象
+
+//普通函数
+void print01(int val)
+{
+	cout << val << " ";
+}
+
+//仿函数
+class print02
+{
+public:
+	void operator()(int val)
+	{
+		cout << val << " ";
+	}
+};
+
+void test01()
+{
+	vector<int>v;
+	for (int i = 0; i < 10; i++)
+	{
+		v.push_back(i);
+	}
+
+	for_each(v.begin(), v.end(), print01);
+	cout << endl;
+
+	for_each(v.begin(), v.end(), print02());
+	cout << endl;
+
+}
+
+int main1()
+{
+
+	test01();
+
+	system("pause");
+
+	return 0;
+}
